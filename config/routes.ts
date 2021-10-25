b9bafb1012fb671b359a0c41c04548a427052d49
zhanggy <geodaoyu@foxmail.com>
2021-10-25 10:10:47 +0800

    chore: 移除国际化

---
+++ config/routes.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      { path: '/user', routes: [{ name: '登录', path: '/user/login', component: './user/Login' }] },
+      { component: './404' },
+    ],
+  },
+  { path: '/home', layout: false, component: './Home' },
+  { path: '/shp', layout: false, component: './Shp' },
+  { path: '/gdb', layout: false, component: './GDB' },
+  { path: '/mul', layout: false, component: './Mul' },
+  { path: '/', redirect: '/home' },
+  { component: './404' },
+];
