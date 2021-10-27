5e5888ac48387e64d6b2853f9bfcdfbfc19b3a2f
zhanggy <geodaoyu@foxmail.com>
2021-10-27 10:46:04 +0800

    feat: 获取文件地址

--- src/components/DraggerUpload/index.js
+++ src/components/DraggerUpload/index.js
@@ -3,14 +3,35 @@ import { InboxOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 
 export default () => {
-  const fileText = useState('aaaaaa');
+  const fileText = useState('');
   const onClick = () => {
-    console.log('click');
+    const { ipcRenderer } = window.electron;
+    ipcRenderer.send('openFile');
+    ipcRenderer.once('filePaths', (event, data) => {
+      console.log(data);
+    });
+  };
+  const onDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const onDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    for (const file of e.dataTransfer.files) {
+      const { path } = file;
+      console.log(path);
+    }
   };
 
   return (
     <Form.Item name="fileUrl">
-      <div className="ant-upload ant-upload-drag" onClick={onClick}>
+      <div
+        className="ant-upload ant-upload-drag"
+        onClick={onClick}
+        onDragOver={onDragOver}
+        onDrop={onDrop}
+      >
         <span tabIndex="0" className="ant-upload ant-upload-btn" role="button">
           <div className="ant-upload-drag-container">
             <p className="ant-upload-drag-icon">
@@ -20,7 +41,7 @@ export default () => {
           </div>
         </span>
       </div>
-      <div className="ant-upload-list ant-upload-list-text">
+      {/* <div className="ant-upload-list ant-upload-list-text">
         <div className="ant-upload-list-text-container">
           <div className="ant-upload-list-item ant-upload-list-item-done ant-upload-list-item-list-type-text">
             <div className="ant-upload-list-item-info">
@@ -30,7 +51,7 @@ export default () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </Form.Item>
   );
 };
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, dialog, ipcRenderer } = require('electron');
 const path = require('path');
 const isDev = process.env.UMI_ENV === 'dev';
 
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       webSecurity: false,
+      contextIsolation: false,
     },
   });
 
@@ -32,6 +33,12 @@ const createWindow = () => {
 
   Menu.setApplicationMenu(null);
 };
+
+ipcMain.on('openFile', (event) => {
+  dialog.showOpenDialog({}).then((result) => {
+    event.sender.send('filePaths', result.filePaths);
+  });
+});
 
 // 这段程序将会在 Electron 结束初始化
 // 和创建浏览器窗口的时候调用
