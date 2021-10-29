a49e423c27ac6b4a6bb26004b8bf55b731694168
zhanggy <geodaoyu@foxmail.com>
2021-10-29 12:04:35 +0800

    feat: 默认全选

--- src/components/ConfigFeaturesTable/index.js
+++ src/components/ConfigFeaturesTable/index.js
@@ -1,26 +1,9 @@
 import ProTable, { TableDropdown } from '@ant-design/pro-table';
 import { EllipsisOutlined, QuestionCircleOutlined, SearchOutlined } from '@ant-design/icons';
-import { message, Layout, Form, Tooltip, Dropdown, Space, Input, Button } from 'antd';
+import { message, Layout, Form, Tooltip, Table, Space, Input, Button } from 'antd';
 import { useEffect, useState, useRef, useCallback } from 'react';
 import { getFeatures, getFieldsInfo } from './service';
 import { filter, map, pipe, fromPairs, addIndex, tap } from 'ramda';
-
-const mapIndexed = addIndex(map);
-
-/**
- * 生成可控的列配置
- * @param {Array} fields 属性数组
- * @returns {Object} columnsStateMap
- */
-const genColumnsStateMap = pipe(
-  mapIndexed(({ fieldName }, index) => {
-    if (index <= 4) {
-      return [fieldName, { show: true }];
-    }
-    return [fieldName, { show: false }];
-  }),
-  fromPairs,
-);
 
 /**
  * custom form item
@@ -29,44 +12,58 @@ const genColumnsStateMap = pipe(
  */
 export default ({ value, onChange, path }) => {
   const [columns, setColumns] = useState([]);
-  const [columnsStateMap, setColumnsStateMap] = useState({});
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
   const searchInput = useRef(null);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
-  const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
+  const rowSelection = {
+    onChange: setSelectedRowKeys,
+    selectedRowKeys,
+    fixed: true,
   };
 
-  const handleReset = (clearFilters) => {
-    clearFilters();
-    setSearchText('');
+  const request = async (params, sorter, filter) => {
+    if (params.path) {
+      return getFeatures(params);
+    }
+    return Promise.resolve({
+      data: [],
+      success: true,
+    });
   };
+
   useEffect(() => {
     const getColumnSearchProps = (dataIndex) => ({
       filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
         <div style={{ padding: 8 }}>
           <Input
             ref={searchInput}
-            placeholder={`Search ${dataIndex}`}
+            placeholder={`请输入 ${dataIndex}`}
             value={selectedKeys[0]}
             onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            onPressEnter={() => {
+              confirm();
+            }}
             style={{ marginBottom: 8, display: 'block' }}
           />
           <Space>
             <Button
               type="primary"
-              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              onClick={() => {
+                confirm();
+              }}
               icon={<SearchOutlined />}
               size="small"
               style={{ width: 90 }}
             >
               查询
             </Button>
-            <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            <Button
+              onClick={() => {
+                clearFilters();
+              }}
+              size="small"
+              style={{ width: 90 }}
+            >
               重置
             </Button>
           </Space>
@@ -109,25 +106,25 @@ export default ({ value, onChange, path }) => {
     if (path) {
       getFieldsInfo({ path }).then(({ fgdbfieldDetailInfos }) => {
         setColumns(genColumns(fgdbfieldDetailInfos));
-        setColumnsStateMap(genColumnsStateMap(fgdbfieldDetailInfos));
       });
     }
   }, [path]);
+
+  useEffect(() => {
+    if (path) {
+      getFeatures({ path }).then(({ data }) => {
+        const keys = data.map(({ id }) => id);
+        setSelectedRowKeys(keys);
+      });
+    }
+  }, [path]);
+
   return (
     <ProTable
-      style={{ width: '800px' }}
+      style={{ width: 800 }}
       columns={columns}
       params={{ path }}
-      request={async (params, sorter, filter) => {
-        console.log(params, sorter, filter);
-        if (params.path) {
-          return getFeatures(params);
-        }
-        return Promise.resolve({
-          data: [],
-          success: true,
-        });
-      }}
+      request={request}
       search={false}
       rowKey="id"
       pagination={{
@@ -135,15 +132,18 @@ export default ({ value, onChange, path }) => {
         pageSize: 5,
         pageSizeOptions: [5, 10, 20, 50, 100],
       }}
+      rowSelection={rowSelection}
       dateFormatter="string"
       toolbar={{
         title: '配置数据',
         tooltip: '在这里配置数据的可见性',
       }}
       columnsState={{
-        value: columnsStateMap,
-        onChange: setColumnsStateMap,
+        onChange: (value) => {
+          console.log(value);
+        },
       }}
+      scroll={{ x: 800 }}
     />
   );
 };
