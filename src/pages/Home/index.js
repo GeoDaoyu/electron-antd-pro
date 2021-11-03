d27d4ef30b8919f83c66cfe6ba4b51e90b0486d6
zhanggy <geodaoyu@foxmail.com>
2021-11-03 17:51:59 +0800

    feat: 首页样式

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
-import { Link } from 'umi';
+import { history } from 'umi';
 import { Layout, Space, Typography } from 'antd';
-import { FileProtectOutlined, DatabaseOutlined, CopyOutlined } from '@ant-design/icons';
 import styles from './index.less';
 
 const { Content } = Layout;
@@ -8,27 +7,30 @@ const { Title } = Typography;
 const routes = [
   {
     to: '/shp',
-    icon: <FileProtectOutlined className={styles['link-icon']} />,
-    label: 'shp数据加密',
+    icon: '/Shp.svg',
+    label: 'Shp数据加密',
   },
   {
     to: '/gdb',
-    icon: <DatabaseOutlined className={styles['link-icon']} />,
+    icon: '/GDB.svg',
     label: 'GDB数据加密',
   },
 ];
 export default () => {
+  const onClick = (to) => {
+    history.push(to);
+  };
   return (
     <Layout className={styles.layout}>
       <Content className={styles.content}>
         <Space size={100}>
           {routes.map((route) => (
-            <Link key={route.to} to={route.to} className={styles.link}>
-              {route.icon}
+            <div key={route.to} className={styles['link']} onClick={() => onClick(route.to)}>
+              <img src={route.icon} />;
               <Title level={2} className={styles['link-title']}>
                 {route.label}
               </Title>
-            </Link>
+            </div>
           ))}
         </Space>
       </Content>
